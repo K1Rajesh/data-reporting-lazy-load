37849beb747edbb4e3686054795e69a935caaae5
Rahul Eppa <rahul.rowthi@gmail.com>
2023-09-22 12:43:04 +0530

    indexing ifreame dev config

--- src/app/features-shared/services/config.service.ts
+++ src/app/features-shared/services/config.service.ts
@@ -18,27 +18,27 @@ export class ConfigService{
       this.subscribeSourceConfig()
     }
     public getSourceConfig():Observable<ConfigModel>{
-        return of(          {
-          "ufillHirearchyMaps": {
-            "Repeat Customers - State": "https://reports.bpcl.co.in/kibana/app/dashboards#/view/1e2415f0-c31e-11ed-889b-ab4d283f8ed1?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-10y%2Cto%3Anow))",
-            "Repeat Customers - Territory": "https://reports.bpcl.co.in/kibana/app/dashboards#/view/ddb1e860-c31f-11ed-889b-ab4d283f8ed1?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-10y%2Cto%3Anow))",
-            "Ticket Size - Overall": "https://reports.bpcl.co.in/kibana/app/dashboards#/view/ceddcc20-c322-11ed-889b-ab4d283f8ed1?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-10y%2Cto%3Anow))",
-            "Ticket Size - State": "https://reports.bpcl.co.in/kibana/app/dashboards#/view/86eeab90-c328-11ed-889b-ab4d283f8ed1?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-10y%2Cto%3Anow))",
-            "Ticket Size - Territory": "https://reports.bpcl.co.in/kibana/app/dashboards#/view/9bf55f80-c32c-11ed-889b-ab4d283f8ed1?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-10y%2Cto%3Anow))",
-            "Top 100 Customers -Territory wise": "https://reports.bpcl.co.in/kibana/app/dashboards#/view/78d56260-c7e2-11ed-889b-ab4d283f8ed1?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-10y%2Cto%3Anow))",
-            "QR Scan vs Manual Entry - RO wise": "https://reports.bpcl.co.in/kibana/app/dashboards#/view/bbd1a950-c7f8-11ed-889b-ab4d283f8ed1?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-10y%2Cto%3Anow))",
-            "Repeat vs New Customers": "https://reports.bpcl.co.in/kibana/app/dashboards#/view/03dfc530-cc75-11ed-9daa-bdee6d9e23f4?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-10y%2Cto%3Anow))",
-            "RO wise bucketing": "https://reports.bpcl.co.in/kibana/app/dashboards#/view/94e1f470-cc8b-11ed-9daa-bdee6d9e23f4?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-10y%2Cto%3Anow))",
-            "UFILL Performance - Monthwise": "https://reports.bpcl.co.in/kibana/app/dashboards#/view/51380250-f48b-11ed-aef9-c57dd217777d?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-10y%2Cto%3Anow))",
-            "UFILL Performance - Summary": "https://reports.bpcl.co.in/kibana/app/dashboards#/view/f0ed4e50-f9ee-11ed-aef9-c57dd217777d?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-10y%2Cto%3Anow))",
-            "UFILL Performance - Datewise": "https://reports.bpcl.co.in/kibana/app/dashboards#/view/7170c510-10f2-11ee-bcb2-9f8c747241b5?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-5y%2Cto%3Anow))",
-            "ACoE - UFill Dashboards": "https://analytics.bpcl.co.in/adfs/sense/app/f19d5241-b6a3-4387-bedd-036a7d7e6d39/sheet/c315ab1f-7d67-4c17-9dc9-5bbc60e3e500/state/analysis"
-          },
-          "ligHirearchyMaps":{
-            "LIG Monthly Control Report" : "https://reports.bpcl.co.in/kibana/app/dashboards#/view/071fad20-2218-11ee-adf4-fb0210763cf7?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-10y%2Cto%3Anow))"
-          },
-          "indexingReport": "https://reports.bpcl.co.in/kibana/app/dashboards#/view/71810be0-fee2-11ed-9b6d-2baf9fac0e11?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-1w%2Cto%3Anow))&show-time-filter=true"
-        })
+        return of({
+            "ufillHirearchyMaps": {
+              "Repeat Customers - State": "https://reportsdev.bpcl.co.in/kibana/app/dashboards?security_tenant=global#/view/44226cd0-6571-11ed-8e7c-cb76ba07d6cb?embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-1y,to:now))&_a=(description:'',filters:!(),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!f),query:(language:kuery,query:''),timeRestore:!t,title:'UFill%20Repeat%20Customer%20Data',viewMode:view)",
+              "Repeat Customers - Territory": "https://reportsdev.bpcl.co.in/kibana/app/dashboards?security_tenant=global#/view/f95d2490-9baa-11ed-a1e6-cb478def64da?embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-1y,to:now))&_a=(description:'',filters:!(),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!f),query:(language:kuery,query:''),timeRestore:!t,title:'UFill%20Repeat%20Customer%20Data%20Territory',viewMode:view)",
+              "Ticket Size - Overall": "https://reportsdev.bpcl.co.in/kibana/app/dashboards?security_tenant=global#/view/db496af0-6fbc-11ed-8961-a337275ba891?embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-1y,to:now))&_a=(description:'',filters:!(),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!f),query:(language:kuery,query:''),timeRestore:!f,title:'UFill%20Ticket%20Size%20Overall',viewMode:view)",
+              "Ticket Size - State": "https://reportsdev.bpcl.co.in/kibana/app/dashboards?security_tenant=global#/view/411252c0-a2df-11ed-a1e6-cb478def64da?embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-1y,to:now))&_a=(description:'',filters:!(),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!f),query:(language:kuery,query:''),timeRestore:!f,title:'UFill%20Ticket%20Size%20Data%20State',viewMode:view)",
+              "Ticket Size - Territory": "https://reportsdev.bpcl.co.in/kibana/app/dashboards?security_tenant=global#/view/4c111cb0-a2df-11ed-a1e6-cb478def64da?embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-1y,to:now))&_a=(description:'',filters:!(),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!f),query:(language:kuery,query:''),timeRestore:!f,title:'UFill%20Ticket%20Size%20Data%20Territory',viewMode:view)",
+              "Top 100 Customers -Territory wise": "https://reportsdev.bpcl.co.in/kibana/app/dashboards?security_tenant=global#/view/78d56260-c7e2-11ed-889b-ab4d283f8ed1?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-1y%2Cto%3Anow))",
+              "QR Scan vs Manual Entry - RO wise": "https://reportsdev.bpcl.co.in/kibana/app/dashboards?security_tenant=global#/view/bbd1a950-c7f8-11ed-889b-ab4d283f8ed1?embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-10y,to:now))&_a=(description:'',filters:!(),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!f),query:(language:kuery,query:''),timeRestore:!f,title:'QR%20Scan%20vs%20Manual%20Entry-RO%20wise',viewMode:view)",
+              "Repeat vs New Customers": "https://reports.bpcl.co.in/kibana/app/dashboards#/view/03dfc530-cc75-11ed-9daa-bdee6d9e23f4?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-10y%2Cto%3Anow))",
+              "RO wise bucketing": "https://reportsdev.bpcl.co.in/kibana/app/dashboards?security_tenant=global#/view/18021c30-cdf0-11ed-867b-39b422af87e1?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-10y%2Cto%3Anow))",
+              "UFILL Performance - Monthwise": "https://reportsdev.bpcl.co.in/kibana/app/dashboards?security_tenant=global#/view/51380250-f48b-11ed-aef9-c57dd217777d?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-10y%2Cto%3Anow))",
+              "UFILL Performance - Summary": "https://reportsdev.bpcl.co.in/kibana/app/dashboards?security_tenant=global#/view/f0ed4e50-f9ee-11ed-aef9-c57dd217777d?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-10y%2Cto%3Anow))",
+              "UFILL Performance - Datewise": "https://reportsdev.bpcl.co.in/kibana/app/dashboards?security_tenant=global#/view/7170c510-10f2-11ee-bcb2-9f8c747241b5?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-10y%2Cto%3Anow))",
+              "ACoE - UFill Dashboards": "https://analytics.bpcl.co.in/adfs/sense/app/f19d5241-b6a3-4387-bedd-036a7d7e6d39/sheet/c315ab1f-7d67-4c17-9dc9-5bbc60e3e500/state/analysis"
+            },
+            "ligHirearchyMaps":{
+              "LIG Monthly Control Report" : "https://reportsdev.bpcl.co.in/kibana/app/dashboards?security_tenant=global#/view/071fad20-2218-11ee-adf4-fb0210763cf7?embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-10y,to:now))&_a=(description:'',filters:!(),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),query:(language:kuery,query:''),timeRestore:!f,title:LIG_Report_1,viewMode:view)"
+            },
+            "indexingReport": "https://reportsdev.bpcl.co.in/kibana/app/dashboards?security_tenant=global#/view/71810be0-fee2-11ed-9b6d-2baf9fac0e11?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-1y%2Cto%3Anow))&show-time-filter=true"
+          })
         //return this.httpClinet.get<ConfigModel>(this.ufillLink);
     }
     subscribeSourceConfig(){
