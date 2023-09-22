16d6bcf30e874d9f3f611200127669f8cb44967b
Rahul Eppa <rahul.rowthi@gmail.com>
2023-09-22 12:49:41 +0530

    adding user-details

---
+++ src/app/authorized-user/domain/model/user-details.model.ts
@@ -0,0 +1,55 @@
+import { Injectable,EventEmitter } from "@angular/core";
+import { BehaviorSubject, Subscription } from "rxjs";
+import { distinctUntilChanged } from 'rxjs/operators';
+
+import { AuthService } from "./../../../services/auth.service";
+
+export type userDetailsIModel = {
+    tenantId: string,
+    objectIdentifier: string,
+    emailAddress: string,
+    displayName: string,
+    displayName2: string,
+    givenName: string,
+    nameID: string,
+    sessionIndex: string,
+    isPrivilegeUser: boolean
+}
+
+@Injectable({
+    providedIn:'root'
+})
+export class UserDetailsModel{
+    private subsList:Array<Subscription> = new Array<Subscription>();
+    private _userDetails : userDetailsIModel | undefined = undefined;
+    public userDetailsEvent : BehaviorSubject<userDetailsIModel| undefined> = new BehaviorSubject<userDetailsIModel| undefined>(undefined)
+    public get userDetails(): userDetailsIModel | undefined {
+        return this._userDetails;
+    }
+    constructor(private authService:AuthService){
+
+    }
+    public init():void{
+        this.subscribeAuthenticatedEvent()
+    }
+    public subscribeAuthenticatedEvent():void{
+        this.subsList.push(
+            this.authService.userSuccessfullyAuthenticatedEvent
+            .pipe(distinctUntilChanged())
+            .subscribe((authResp:boolean)=>{
+                if(authResp ){
+                    this.getUserDetails()
+                }   
+        }) 
+        )
+    }
+    public getUserDetails() : void {
+        this.authService.getUserDetails().subscribe((userDetails: userDetailsIModel)=>{
+            this._userDetails = userDetails;
+            this.userDetailsEvent.next(this.userDetails)
+        })
+    }
+    public destroy():void{
+     this.subsList.forEach(sub=>sub.unsubscribe())   
+    }
+}
