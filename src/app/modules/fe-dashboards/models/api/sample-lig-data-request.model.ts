3b3f3aa17025ca04980d5b805dc608ca6c7a1ae0
Rahul Eppa <rahul.rowthi@gmail.com>
2023-10-05 13:42:33 +0530

    updating request mdel in sample request

--- src/app/modules/fe-dashboards/models/api/sample-lig-data-request.model.ts
+++ src/app/modules/fe-dashboards/models/api/sample-lig-data-request.model.ts
@@ -1,14 +1,22 @@
 import { LigDataRequestIModel } from './lig-data-request.model';
 
 export const MOCK_LIG_DATA_REQUEST: LigDataRequestIModel = {
-    "user": {
-      "email": "abhisekdatta@corp.bharatpetroleum.com"
-    },
-    "filters": {
-      "month": "2023-08",
-      "user_persona": "lubes_personalvehicleowner",
-      "taluka": "Kesinga",
-      "sap_cc_number": "117606.0",
-      "SALES_GROUP_NAME": "Rourkela Lubes"
-    }
+  "user": {
+    "email": "anushigupta@corp.bharatpetroleum.com"
+  },
+  "provideData":true,
+  "filters": {
+    "month": "2023-08",
+    "user_persona": ["lubes_personalvehicleowner"],
+    "taluka": ["Kesinga"],
+    "sap_cc_number": ["117606.0"],
+    "SALES_GROUP_NAME": ["Rourkela Lubes"],
+    "SALES_OFFICE_NAME": ["Mumbai Territory"],
+    "state": ["Maharashtra"],
+    "district": ["MUMBAI"],
+    "PRODUCT_NAME": ["MAK 4T Plus (SL 20W-40) 1 lt X 12"],
+    "PRODUCT_CODE": ["7265857"],
+    "PRODUCT_BRAND": ["4 T PLUS"]
+    
+  }
 }
