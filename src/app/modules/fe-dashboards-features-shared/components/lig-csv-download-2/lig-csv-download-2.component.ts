f42fb3dd5c136a95a7d6de7323ba0edb84806fd8
Rahul Eppa <rahul.rowthi@gmail.com>
2023-10-26 13:39:57 +0530

    creating another download component

---
+++ src/app/modules/fe-dashboards-features-shared/components/lig-csv-download-2/lig-csv-download-2.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription, Observable, pipe } from 'rxjs';
+import { take } from 'rxjs/operators';
+
+import { LigCsvDownloadService } from './../../services/lig-csv-download.service';
+
+import { LigDashboardTableApiHeaders, LigDashboardTableHeadersApiMapping } from './../../models/lig-dashboard-data.model';
+
+//* FEDashbaords Module*//
+import {LigDataResponseIModel} from "../../../fe-dashboards/models/api/lig-data-reponse.model";
+
+import { LigDataModel} from "../../../fe-dashboards/domain/models/lig-data.model";
+
+@Component({
+  selector: 'app-lig-csv-download-2',
+  templateUrl: './lig-csv-download-2.component.html',
+  styleUrls: ['./lig-csv-download-2.component.css']
+})
+export class LigCsvDownloadComponent2 implements OnInit, OnDestroy {
+
+
+  public isDataLoading: boolean = false;
+  public csvDownloadHeaders: Array<string> = new Array<string>();
+  private subsList : Array<Subscription> = new Array<Subscription>();
+
+
+  constructor(private ligCsvDownloadService : LigCsvDownloadService,
+    private ligDataModel : LigDataModel ){
+    console.log("At LigCsvDownloadComponent constructor");
+  }
+  ngOnInit(){
+    this.csvDownloadHeaders = LigDashboardTableApiHeaders
+  }
+
+  public downloadClickHandler():void{
+
+    this.isDataLoading = true;
+    
+    this.ligDataModel.getLigDataResponse()
+    .subscribe((ligDataResponse:LigDataResponseIModel | undefined)=>{
+        if(ligDataResponse?.success && ligDataResponse?.provideData && ligDataResponse?.data){
+          this.isDataLoading = false;
+          this.ligCsvDownloadService.downloadFile(ligDataResponse.data, this.csvDownloadHeaders, LigDashboardTableHeadersApiMapping );
+        }
+      },
+      (error)=>{
+        this.isDataLoading = false;
+      }
+    )
+    .unsubscribe();
+
+  }
+  ngOnDestroy():void{
+    this.subsList.forEach((sub)=>sub.unsubscribe());
+  }
+
+}
